75ccdfc80c5d001e3e0c2d9c8cd329aa8e427d79
Italo Oliveira <italooliveira-dev@hotmail.com>
2024-09-23 21:47:13 -0300

    Revert :arrow_left: "revertendo feat: adicionando e configurando rotas com react router dom devido a adição de arquivos errados"
    This reverts commit ac928bb0f346a22d4ee132ade8edb89b71a1f1f0.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { ThemeProvider } from 'styled-components';
+import { Button } from './components/Button';
 import { defaultTheme } from './styles/themes/default';
 import { StyleGlogal } from './styles/global';
-import { BrowserRouter } from 'react-router-dom';
-import { Router } from './Router';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+
       <StyleGlogal />
     </ThemeProvider>
   );
