2287358323b529c1e72c6b48c84ccc3c5b381c21
Italo Oliveira <italooliveira-dev@hotmail.com>
2024-09-23 22:39:32 -0300

    feat(router): :truck: configurando  rotas com react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styles/themes/default';
 import { StyleGlogal } from './styles/global';
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './Router';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <StyleGlogal />
     </ThemeProvider>
   );
