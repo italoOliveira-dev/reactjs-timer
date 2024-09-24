f7940373a350f2ae6c540788a69971ee781a6bbd
Italo Oliveira <italooliveira-dev@hotmail.com>
2024-09-23 22:13:17 -0300

    refactor: :triangular_flag_on_post: removendo componente Button onde não será utilizado.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import { ThemeProvider } from 'styled-components';
-import { Button } from './components/Button';
 import { defaultTheme } from './styles/themes/default';
 import { StyleGlogal } from './styles/global';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-
       <StyleGlogal />
     </ThemeProvider>
   );
